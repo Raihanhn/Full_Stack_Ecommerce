47a3423b7a6631709474697bc0a6bc964edbc298
Mohammad Raihan <108555328+Raihanhn@users.noreply.github.com>
2025-09-11 15:04:10 +0600

    product save in cart even refresh using redux persist

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
-import { store } from "@/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "@/redux/store";
 import Navbar from "@/components/Navbar";
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
+       <PersistGate loading={null} persistor={persistor}>
       <Navbar />
       <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   );
 }
--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // uses localStorage for web
 import cartReducer from "./cartSlice";
 
-export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
+// Combine reducers (in case you add more later)
+const rootReducer = combineReducers({
+  cart: cartReducer,
 });
+
+// Config for persistence
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// Wrap root reducer with persistReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create store
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+// Create persistor
+export const persistor = persistStore(store);
